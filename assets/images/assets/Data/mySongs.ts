18c82b5085d7dca8eefb3e69862a10f42883f34f
ResilientProgramer <52796008+ResilientProgramer@users.noreply.github.com>
2019-07-26 00:03:30 -0400

    Add files via upload

---
+++ assets/images/assets/Data/mySongs.ts
@@ -0,0 +1,8 @@
+import{SongInfo} from '../../app/SongInfo';
+
+export const MYSONGS: SongInfo[] = [
+    {songname:'Come What May', artist:'Air Supply', genre:'Vocal/Easy Listening', yreleased: 1982,picture:'../assets/images/comewhatmay.jpg'},
+    {songname:'Making Love Out Of Nothing At All', artist:'Air Supply', genre:'Pop', yreleased: 1983,picture:'../assets/images/makingloveoutofnothingatall.jpg'},
+    {songname:'I Can Wait Forever', artist:'Air Supply', genre:'Pop', yreleased: 1984,picture:'../assets/images/icanwaitforever.jpg'},
+    {songname:'If You Go Away', artist:'Frank Sinatra', genre:'Jazz', yreleased: 1966,picture:'../assets/images/ifyougoaway.jpg'}
+]
